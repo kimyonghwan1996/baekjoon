0067adadb19dab2bc5cf70759e357a6e9ac13e46
kimyonghwan1996 <137673370+kimyonghwan1996@users.noreply.github.com>
2024-02-08 09:32:48 +0900

    [Silver II] Title: 최대 힙, Time: 1748 ms, Memory: 110848 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,25 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int i = 0; i < N; i++) {
+            int x = scanner.nextInt();
+            if (x == 0) {
+                if (minHeap.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(minHeap.poll());
+                }
+            } else {
+                minHeap.offer(x);
+            }
+        }
+        scanner.close();
+    }
+}
